c23ff5d80ca14669bdf3a57b774d63e4a379db6c
kuzeyfe <50907921+kuzeyfe@users.noreply.github.com>
2023-05-15 03:40:36 +0300

    1.16
    1.16

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
-import 'package:deneme3/login_page.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-import 'login_page.dart';
-import 'register_page.dart';
-import 'landing_page.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(MyApp());
 }
 
@@ -17,16 +9,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My App',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LandingPage(),
-      routes: <String, WidgetBuilder>{
-        '/landingpage': (BuildContext context) => new LandingPage(),
-        '/registerpage': (BuildContext context) => new RegisterPage(),
-        '/loginpage': (BuildContext context) => new LoginPage(),
-      },
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: Center(
+        child: Text('Hello, world!'),
+      ),
     );
   }
 }
